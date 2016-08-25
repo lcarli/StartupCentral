451fd1c4e131bce84f23a21084683526e86ef4f9
Lucas <lucas.decarli@gmail.com>
2016-08-25 18:24:45 -0300

    Ajustando algumas telas e o Layout principal

---
+++ StartupCentral/Controllers/StartupController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StartupCentral.Models;
+
+namespace StartupCentral.Controllers
+{
+    public class StartupController : Controller
+    {
+        private StartupDBContext db = new StartupDBContext();
+
+        // GET: Startup
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Startup.ToListAsync());
+        }
+
+        // GET: Startup/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Startupbs startupbs = await db.Startup.FindAsync(id);
+            if (startupbs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(startupbs);
+        }
+
+        // GET: Startup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Startup/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,nome,email,msa,BizSparkID,ConsumoMes,ConsumoAcumulado,ConsumoPago")] Startupbs startupbs)
+        {
+            if (ModelState.IsValid)
+            {
+                startupbs.ID = Guid.NewGuid();
+                db.Startup.Add(startupbs);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(startupbs);
+        }
+
+        // GET: Startup/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Startupbs startupbs = await db.Startup.FindAsync(id);
+            if (startupbs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(startupbs);
+        }
+
+        // POST: Startup/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,nome,email,msa,BizSparkID,ConsumoMes,ConsumoAcumulado,ConsumoPago")] Startupbs startupbs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(startupbs).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(startupbs);
+        }
+
+        // GET: Startup/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Startupbs startupbs = await db.Startup.FindAsync(id);
+            if (startupbs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(startupbs);
+        }
+
+        // POST: Startup/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Startupbs startupbs = await db.Startup.FindAsync(id);
+            db.Startup.Remove(startupbs);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

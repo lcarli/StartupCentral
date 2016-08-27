13b71515f3da239d9fdc75da50fccc4397b54642
Lucas <lucas.decarli@gmail.com>
2016-08-27 01:30:11 -0300

    Ajustando alguns gráficos e Widgets

--- StartupCentral/Controllers/HomeController.cs
+++ StartupCentral/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace StartupCentral.Controllers
 
         public async Task<ActionResult> Index()
         {
+            //GetUser
             var cp = ClaimsPrincipal.Current.Identities.First();
             Models.User user = new Models.User() { ID = Guid.NewGuid(), nome = cp.Claims.First(c => c.Type == "name").Value, email = cp.Name };
             ViewBag.Nome = user.nome.ToString();
@@ -28,6 +29,13 @@ namespace StartupCentral.Controllers
             {
                 db.LogLogin.Add(new Models.LogLogin() { datetime = DateTime.Now, user = user });
             }
+
+            //Complete Dashboards
+            countStartupsBS();
+            countStartupsBSPlus();
+            countStartupsNews();
+            SumConsumoTotal();
+
             return View();
         }
 
@@ -43,6 +51,34 @@ namespace StartupCentral.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public void countStartupsBS()
+        {
+            ViewBag.NumStartupsBS = "235";
+        }
+
+        public void countStartupsBSPlus()
+        {
+            @ViewBag.NumStartupsBSPlus = "18";
+        }
+
+        public void countStartupsNews()
+        {
+            @ViewBag.NumNews = "20";
+        }
+        public void SumConsumoTotal()
+        {
+            @ViewBag.ConsumoTotal = "$20.000";
+        }
+
+        public ActionResult JsonValues()
+        {
+            Random randNums = new Random();
+            var randNum = randNums.Next(10, 20);
+            return Json(
+                new[] { new[] { new DateTime().Day, randNum } },
+                JsonRequestBehavior.AllowGet);
         }
     }
 }
